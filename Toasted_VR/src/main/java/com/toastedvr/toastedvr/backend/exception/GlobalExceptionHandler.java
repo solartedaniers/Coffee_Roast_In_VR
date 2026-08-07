e39694a4904ebd16d9dc4b52d0421b72058d7652
Daniers <solartedaniers@gmail.com>
2026-08-07 11:31:42 -0500

    Elimino un import que x la actualizacion anterior ya no era nesesario

--- Toasted_VR/src/main/java/com/toastedvr/toastedvr/backend/exception/GlobalExceptionHandler.java
+++ Toasted_VR/src/main/java/com/toastedvr/toastedvr/backend/exception/GlobalExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
